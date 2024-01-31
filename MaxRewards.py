d579e5206eac8738fdbf4edb3acc7e5eee9dee35
bylaye <abdoulayeniang2010@gmail.com>
2024-01-31 16:28:30 +0000

    added function to get itineraries and movements

--- MaxRewards.py
+++ MaxRewards.py
@@ -3,16 +3,18 @@
 """
 
 class MaxRewards:
+
 	def __init__(self, matrix, start, end):
 		self.matrix = matrix
 		self.start = start
 		self.end = end
 		check = self._check_params()
 		if check[0]:
-			print(check[1])
-			pass
+			self.max_rewards, self.itineraries = self._process()
+			self.movements = self._movements(self.itineraries)
 		else:
 			print(check[1])
+			print(f'Exit program')
 			exit (0)
 	
 	def _check_entry(self, entry, values_checking):
@@ -43,12 +45,10 @@ class MaxRewards:
 			checking = False
 		return checking, comment
 		
-	def process(self):
+	def _process(self):
 		sx, sy = self.start
 		ex, ey = self.end
 		ex, ey = ex+1, ey+1
-		#reward_tab = [[0]*self.N_COLS for _ in range(self.N_ROWS)]
-		#path = [[None]*self.N_COLS for _ in range(self.N_ROWS)]
 		reward_tab = [[0]*ey for _ in range(ex)]
 		path = [[None]*ey for _ in range(ex)]
 		reward_tab[sx][sy] = self.matrix[sx][sy]
@@ -59,6 +59,37 @@ class MaxRewards:
 				m = max(left, top)
 				reward_tab[i][j] = m[0] + self.matrix[i][j]
 				path[i][j] = m[1]
-		return reward_tab, path
+		itineraire = self._itineraries(path)
+		return reward_tab[-1][-1], itineraire
 		
-			
+	def _itineraries(self, path):
+		out = [self.end]
+		ex, ey = self.end
+		pred = (ex, ey)
+		while pred != self.start:
+			c = path[pred[0]][pred[1]]
+			out.append(c)
+			pred = (c[0],c[1])
+		out.reverse()
+		return out
+		
+	def _movements(self, itineraire):
+		out = 's'
+		pred = itineraire[0]
+		for i in range(1, len(itineraire)):
+		    next = itineraire[i]
+		    if pred[0] == next[0]:
+		        out += '-d'
+		    else:
+		        out += '-b'
+		    pred = next
+		return out
+	
+	def get_max_rewards(self):
+		return self.max_rewards
+		
+	def get_itineraries(self):
+		return self.itineraries
+		
+	def get_movements(self):
+		return self.movements
